c7484abfbb73ed876bba9c715430e5da5a94a37f
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-25 17:20:37 -0300

    feat: added database.js

---
+++ database/database.js
@@ -0,0 +1,18 @@
+import { MongoClient } from 'mongodb'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const mongoClient = new MongoClient(process.env.MONGO_URL)
+
+try {
+    await mongoClient.connect()
+    console.log("Connected to Mongodb.")
+} catch (err) {
+    console.log(err)
+}
+
+const db = mongoClient.db()
+
+export const usersCollection = db.collection('users')
+export const productsCollection = db.collection('products')
+
