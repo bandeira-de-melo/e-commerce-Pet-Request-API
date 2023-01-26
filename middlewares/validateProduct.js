644904df8ddc0f2d43ffdbaa8fcf5d5393a1d90a
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-26 18:22:44 -0300

    fix/ validateProduct middleware

--- middlewares/validateProduct.js
+++ middlewares/validateProduct.js
@@ -2,8 +2,11 @@ import Joi from "joi"
 export const validateProduct = (schema)=>{
     return (req, res, next)=>{
         const product = res.locals.product
+
         const {error, value} = Joi.validate(product, schema, {abortEarly: false })
         if(error) return res.status(422).send(error.details.map(err => err))
+
+        res.locals.product = value
         next()
     }
 }
