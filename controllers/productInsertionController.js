09032bec58ed5c192f8725df4191409cdb94f63b
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-26 17:37:35 -0300

    feat/added product insertion func and dayjs dependency

--- controllers/productInsertionController.js
+++ controllers/productInsertionController.js
@@ -1,9 +1,20 @@
+import { productsCollection } from "../database/database"
+import dayjs from "dayjs"
 
 export const productIsertionController = async (req, res)=>{
     const adminInfo = res.locals.adminInfo
+    const product = res.locals.product
     try {
-        
+        await productsCollection.insertOne({
+            title: product.title,
+            image: product.image,
+            description: product.description,
+            details: product.details,
+            adminId: adminInfo.adminId,
+            createdAt: dayjs().format('MMM D, YYYY h:mm A')
+        })
     } catch (error) {
-        
+        console.error(error)
+        res.sendStatus(500)
     }
 }
