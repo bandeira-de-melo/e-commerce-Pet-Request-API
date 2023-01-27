114d0e19e17fbfe007dd040d4df75fc4d2b904dc
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 10:26:08 -0300

    fix/add verification of credentials in preadmins collection

--- middlewares/adminSignUpValidation.js
+++ middlewares/adminSignUpValidation.js
@@ -1,11 +1,11 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export default function adminSignUpValidation(req, res, next) {
+export const adminSignUpValidation = async (req, res, next) {
     const {username, password} = req.body
 
     if(!username || !password)return res.status(422).send("You must enter an admin username and passoword.")
-
+    
     if(username != process.env.ADMIN_USER && password != process.env.ADMIN_PASS){
         return res.status(401).send("Wrong username or password.")
     }
