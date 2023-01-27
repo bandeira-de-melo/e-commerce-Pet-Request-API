816b7686da04c51766a0dc82be1e04c3f95e0352
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 14:24:11 -0300

    feat/added admin routes

--- routes/adminsRoutes.js
+++ routes/adminsRoutes.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { productIsertionController } from "../controllers/productInsertionController";
 import { adminSignUpValidation } from "../middlewares/adminSignUpValidation";
+import { validateAdmin } from "../middlewares/validateAdmin";
 import { validateProductInsertion } from "../middlewares/validateProductInsertion";
 import { productSchema } from "../schemas/productSchema";
 
 const adminsRouter = Router()
 
 adminsRouter.get('/admins-login')
-adminsRouter.post('/admins-login')
+adminsRouter.post('/admins-login', validateAdmin, )
 adminsRouter.post("/admins-signup", adminSignUpValidation, )
 adminsRouter.post("/insert-product", validateProductInsertion, validateProduct(productSchema), productIsertionController)
 
