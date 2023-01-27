d9292ad6a42d5f7f9812268805ee68268a07829f
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 15:04:35 -0300

    added validateSchema to admins login route

--- routes/adminsRoutes.js
+++ routes/adminsRoutes.js
@@ -3,12 +3,14 @@ import { productIsertionController } from "../controllers/productInsertionContro
 import { adminSignUpValidation } from "../middlewares/adminSignUpValidation";
 import { validateAdmin } from "../middlewares/validateAdmin";
 import { validateProductInsertion } from "../middlewares/validateProductInsertion";
+import { validateSchema } from "../middlewares/validateSchema";
+import { adminLogin } from "../schemas/adminsSchemas";
 import { productSchema } from "../schemas/productSchema";
 
 const adminsRouter = Router()
 
 adminsRouter.get('/admins-login')
-adminsRouter.post('/admins-login', validateAdmin, )
+adminsRouter.post('/admins-login', validateAdmin, validateSchema(adminLogin) )
 adminsRouter.post("/admins-signup", adminSignUpValidation, )
 adminsRouter.post("/insert-product", validateProductInsertion, validateProduct(productSchema), productIsertionController)
 
