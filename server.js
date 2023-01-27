f7fb53cb4e495bc652cb0fb66eed0328d2bd8baf
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 15:37:31 -0300

    added adminsRouter to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors'
 import dotenv from 'dotenv';
+import adminsRouter from './routes/adminsRoutes.js';
 dotenv.config()
 
 const app = express()
@@ -8,6 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use(adminsRouter)
 
 
 app.listen(process.env.PORT, ()=>{
