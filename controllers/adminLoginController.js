918a79c582ddfcb737ef1bea86124b8b78e7d0db
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 15:25:51 -0300

    added adminLoginController

---
+++ controllers/adminLoginController.js
@@ -0,0 +1,19 @@
+import { sessionsAdminCollection } from "../database/database"
+
+export const adminLoginController = async (req, res) =>{
+    const userInAdmins = res.locals.adminInfo
+
+    if(userInAdmins.isAdmin){
+        token = uuidV4()
+        try {
+            await sessionsAdminCollection.insertOne({userId: userInAdmins._id, token, createdAt: dayjs().format('HH:mm:ss') })
+            res.send(200).send(token)
+        } catch (err) {
+            console.error(err)
+            res.status(500).send("Error on the server side.")
+        }
+        
+    } else {
+        res.send("redirectUserSignUp")
+    }
+}
