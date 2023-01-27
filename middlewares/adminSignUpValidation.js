f2199cae2627890947d789147d12bda7f73d3fb1
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 10:41:22 -0300

    fix/ imported preAdmins collection to middleware and refactored adminSignUpValidation  code

--- middlewares/adminSignUpValidation.js
+++ middlewares/adminSignUpValidation.js
@@ -1,14 +1,22 @@
-import dotenv from 'dotenv'
-dotenv.config()
 
-export const adminSignUpValidation = async (req, res, next) {
+import { preAdminsCollection } from "../database/database"
+
+export const adminSignUpValidation = async (req, res, next)=> {
     const {username, password} = req.body
 
     if(!username || !password)return res.status(422).send("You must enter an admin username and passoword.")
     
-    if(username != process.env.ADMIN_USER && password != process.env.ADMIN_PASS){
-        return res.status(401).send("Wrong username or password.")
+    try {
+        const adminIndb = await preAdminsCollection.findOne({username: username})
+        if(!adminIndb) return res.status(401).send("Wrong username or password.")
+        
+        if(password !== adminIndb.password){
+            return res.status(401).send("Wrong username or password.")
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).send("Error on the server side.")
     }
-
+    
     next()
 }
