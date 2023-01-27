499615c26a4120267b1186b71c46f3008d5b1d77
bandeira-de-melo <trabalho.bandeira@gmail.com>
2023-01-27 09:53:18 -0300

    feat/added admins signup and login schemas

---
+++ schemas/adminsSchemas.js
@@ -0,0 +1,13 @@
+import Joi from "joi";
+
+export const adminSignUp = Joi.object({
+    name: Joi.string().min(2).required(),
+    email: Joi.string().min(2).required(),
+    password: Joi.string().min(6).required(),
+    confirm_password: Joi.ref('password')
+})
+
+export const adminLogin = Joi.object({
+    name:Joi.string().min(2).required(),
+    password: Joi.string().min(6).required(),
+})
