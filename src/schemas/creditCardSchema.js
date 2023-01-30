d05d4bf6f5c9b4472e791d34787b429a1342f118
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-01-30 15:23:36 -0300

    feat: credit card schema

--- src/schemas/creditCardSchema.js
+++ src/schemas/creditCardSchema.js
@@ -1,5 +1,8 @@
 import joi from 'joi'
 
-export const cardSchema = joi.string().creditCard();
-
-
+export const cardSchema = joi.object({
+    cardName: joi.string().required(),
+    cardNumber: joi.string().creditCard().required(),
+    expirationDate: joi.date().format("MM/YYYY").required(),
+    cardCVC: joi.string().min(3).required()
+})
