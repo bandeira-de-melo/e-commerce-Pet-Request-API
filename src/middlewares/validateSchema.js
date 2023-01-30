8e3be511c17ca68ef78763e99c86f9b4e0973a35
Ana Laura Ferraz <analaurafferraz@gmail.com>
2023-01-30 13:06:26 -0300

    feat: creating routes cart and checkout

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -1,10 +1,10 @@
 import Joi from "joi"
 
-export const validateSchema = (schema)=>{
-    return (req, res, next)=>{
+export const validateSchema = (schema) => {
+    return (req, res, next) => {
         const data = req.body
-        const {error, value} = Joi.validate(data, schema,{abortEarly: false})
-        if(error){
+        const { error, value } = Joi.validate(data, schema, { abortEarly: false })
+        if (error) {
             const errors = error.details.map(err => err)
             console.log(errors)
             return res.status(403).send("Please send data in correct format.")
